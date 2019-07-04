7457d732f793841d7da499f87e5c72673d1bef57
snower <sujian199@gmail.com>
2019-07-04 17:26:07 +0800

    fix rlock

--- src/RLock.php
+++ src/RLock.php
@@ -17,38 +17,39 @@ class RLock
     protected $db = null;
     protected $db_id = 0;
     protected $lock_name = '';
-    protected $locks = null;
     protected $timeout = 0;
     protected $expried = 0;
+    protected $lock = null;
+    protected $lock_count = 0;
 
     public function __construct($db, $lock_name, $timeout=5, $expried=65, $count=1)
     {
         $this->db = $db;
         $this->db_id = $db->GetDbId();
         $this->lock_name = $lock_name;
-        $this->locks = [];
         $this->timeout = $timeout;
         $this->expried = $expried;
+
+        $this->lock = new Lock($this->db, $this->lock_name, $this->timeout, $this->expried, null, 1, 0xff);
+        $this->lock_count = 0;
     }
 
     public function Acquire()
     {
-        if(count($this->locks) >= 0xff) {
+        if($this->lock_count >= 0xff) {
             throw new LockLockedError();
         }
 
-        $lock = new Lock($this->db, $this->lock_name, $this->timeout, $this->expried, null, 1, 0xff);
-        $lock->Acquire();
-        array_push($this->locks, $lock);
+        $this->lock->Acquire();
+        $this->lock_count++;
     }
 
     public function Release()
     {
-        $lock = array_pop($this->locks);
-        if($lock == null) {
+        if($this->lock_count == 0) {
             throw new LockUnlockedError();
         }
 
-        $lock->Release();
+        $this->lock->Release();
     }
 }
